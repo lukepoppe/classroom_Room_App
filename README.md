d416497822cdd0647ca98c97f879cc031a55c037
mliq <liquori@gmail.com>
2015-06-12 11:25:37 -0500

    Front-end: grid, jquery and bootstrap added, clicking on grid adds occupied class and color

---
+++ README.md
@@ -0,0 +1,3 @@
+# Prime Academy Group Project
+
+
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,8 @@
 console.log('hi');
+
+$(document).ready(function(){
+    $('body').on('click', '.block', function() {
+        console.log($(this).attr('id'));
+        $(this).toggleClass('occupied');
+    });
+});
