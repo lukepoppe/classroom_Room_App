f0aa7eae9963afcb7989b80457c39152c9421846
mliq <liquori@gmail.com>
2015-06-16 14:19:27 -0500

    Dummy data in separate js file

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,17 +4,12 @@ console.log('app.js is loaded');
 var cohortNumber = 0;
 var classroomNumber = 0;
 
-// DUMMY DATA
-var dummyPerson = new Person("Michael Liquori","liquori@gmail.com", "Student", [], {}, []);
-var dummyPerson2 = new Person("Casie Lynch","cds.lynch@gmail.com", "Student", [], {}, []);
-var dummyCohort = new Cohort(0,0,"Bloomington", 2015, 3, [dummyPerson]);
-var dummyCohort2 = new Cohort(0,0,"Bloomington", 2015, 5, [dummyPerson2]);
-var dummyClassroom = new Classroom(1, 1, "Bloomington");
-var dummyClassroom2 = new Classroom(1, 1, "Bloomington");
+// Initialize Arrays of all data (DUMMY data for now, will be from DB)
+classroomArray = [dummyClassroom, dummyClassroom2];
+cohortArray = [dummyCohort, dummyCohort2];
 
-// Initialize Arrays (DUMMY data for now, will be from DB)
-classroomArray = [dummyClassroom,dummyClassroom2];
-currentDeskArray =  classroomArray[0].deskArray;
+// Initialize variables holding the currently being viewed data
+currentDeskArray = classroomArray[0].deskArray;
 
 // Load Fresh Classroom Template Function
 function refreshClassroom() {
