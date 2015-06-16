738f9ee5b5408f4cf96b4d7a03f511ab19e51518
mliq <liquori@gmail.com>
2015-06-16 11:30:10 -0500

    Sign Out functionality added

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -55,4 +55,11 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
 }
+// Google Signout
 
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
