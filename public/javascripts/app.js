611ed814b7e024459914ae8e43867086687789ad
mliq <liquori@gmail.com>
2015-06-16 12:25:16 -0500

    Create new classrooms functionality

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,23 @@
 console.log('hi');
 
-// Set Default Classroom Number to 1
-var classroomNumber=1;
+// Initialize Array of Cohorts
+cohortArray = []; // TO DO: Ajax Call to DB
+
+// QUESTION: CohortArray contains cohort Objects or Arrays? (Which then contains Person Objects.)
+
+// Set Default Cohort Number
+var cohortNumber = 0;
+
+// Initialize Array of Classrooms
+// TO DO: Ajax Call to DB
+// DUMMY data for now.
+classroomArray = [new Classroom(1,1)];
+
+// Set Default Classroom Number to 0
+var classroomNumber=0;
+
+// Initialize Array of Desks from Classroom Number 1
+currentDeskArray = classroomArray[classroomNumber].deskArray;
 
 // Desk Constructor Function
 function Desk(number, position, person, classroom){
@@ -11,15 +27,26 @@ function Desk(number, position, person, classroom){
     this.classroom = classroom;
 }
 
-// Initialize Array of Desks
-deskArray = [];
-
 // Load Fresh Classroom Template Function
 function refreshClassroom() {
     $('.classroom').load('classroom.html');
 }
 
+// Classroom Constructor Function
+function Classroom(number, cohort){
+    this.number = number;
+    this.deskArray = []; // TO DO: Set up a default desk array
+    this.cohort = cohort;
+    this.city = "Bloomington"; // TO DO: Option to change City?
+}
+
+function save(){
+    console.log("Saves everything to db");
+}
 $(document).ready(function(){
+
+    // Load Fresh Classroom Template
+    refreshClassroom();
 
     // On Click of Grid Blocks
     $('body').on('click', '.block', function() {
@@ -28,21 +55,18 @@ $(document).ready(function(){
 
         if($(this).hasClass('occupied')){
             // Erase a desk
-            deskArray = deskArray.filter(function( obj ) {
+            currentDeskArray = currentDeskArray.filter(function( obj ) {
                 return obj.position !== clickedPosition;
             });
 
         } else {
             // Create a desk with .block ID as position attribute.
-            deskArray.push(new Desk(deskArray.length+1, clickedPosition, "", classroomNumber));
+            currentDeskArray.push(new Desk(currentDeskArray.length, clickedPosition, "", classroomNumber));
         }
 
         // Toggle .occupied Class
         $(this).toggleClass('occupied');
     });
-
-    // Load Fresh Classroom Template
-    refreshClassroom();
 
     // On Click of Classroom Selector Links
     $('body').on('click', '.classroomSelector', function() {
@@ -51,6 +75,19 @@ $(document).ready(function(){
         // To do: Load data for classroom
 
     } );
+
+    // On Click of Plus Button (Create New Classroom)
+    $('body').on('click', '.newClassroomButton', function(){
+        refreshClassroom();
+        classroomNumber = classroomArray.length+1;
+        classroomArray.push(new Classroom(classroomNumber, cohortNumber));
+        currentDeskArray = [];
+    });
+
+    // On Click of Save Button
+    $('body').on('click', '.saveButton', function(){
+        save();
+    });
 });
 
 // Google Login Authorization Function
@@ -61,8 +98,8 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
 }
-// Google Signout
 
+// Google Signout
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
