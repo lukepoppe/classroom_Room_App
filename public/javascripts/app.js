bfc58c5ddd98d7e1ccccfe1ada1c0c697329de5b
mliq <liquori@gmail.com>
2015-06-16 11:19:53 -0500

    Desk objects created on click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,22 +1,53 @@
 console.log('hi');
 
-var classroom=1;
+// Set Default Classroom Number to 1
+var classroomNumber=1;
+
+// Desk Constructor Function
+function Desk(number, position, person, classroom){
+    this.number = number;
+    this.position = position;
+    this.person = person;
+    this.classroom = classroom;
+}
+
+// Initialize Array of Desks
+deskArray = [];
+
+// Load Fresh Classroom Template Function
+function refreshClassroom() {
+    $('.classroom').load('classroom.html');
+}
 
 $(document).ready(function(){
+
+    // On Click of Grid Blocks
     $('body').on('click', '.block', function() {
-        console.log($(this).attr('id'));
+
+        // Create a desk with .block ID as position attribute.
+        deskArray.push(new Desk(deskArray.length+1, $(this).attr('id'), "", classroomNumber));
+
+        // Erase a desk?
+
+        // Toggle .occupied Class
         $(this).toggleClass('occupied');
+
+
     });
 
-    $('.classroom').load('classroom.html');
+    // Load Fresh Classroom Template
+    refreshClassroom();
 
+    // On Click of Classroom Selector Links
     $('body').on('click', '.classroomSelector', function() {
-        //console.log($(this).data('classroom'));
-        classroom = $(this).data('classroom');
-        $('.classroom').load('classroom.html');
+        refreshClassroom();
+        classroomNumber = $(this).data('classroom');
+        // To do: Load data for classroom
+
     } );
 });
 
+// Google Login Authorization Function
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
