c6fdd300e2a7bf9f96daa76722ee633ed210b0ea
Casie Lynch <cds.lynch@gmail.com>
2015-06-22 09:08:27 -0500

    updated cohort, still not sending to DB

--- public/javascripts/cohorts.js
+++ public/javascripts/cohorts.js
@@ -13,77 +13,33 @@ $(document).ready(function () {
     });
 
     $('.submit').on("click", function(){
+        $('.showList').show();
         var firstName = $('.firstName').val();
         var lastName = $('.lastName').val();
         var email = $('.email').val();
+        //postData();
 
         console.log(firstName);
-        // for(var i = 0; i < cohort.length; i++) {
-        //    $('.showList').append("<p>" + cohort[i] + "  |  <button class='edit'>Edit</button></p>");
-        //};
+        $('.showList').append("<p>" + firstName + " " + lastName + "  |  " + email +  "  |  <button class='edit'>Edit</button></p>");
     });
-
-    function getData(){
-        $.ajax({
-            url: '/people',
-            method: 'get',
-            data: {},
-            dataType: 'json',
-            success: function(response, textStatus, jqXHR){
-                //clearData();
-                processData(response);
-            },
-            error: function(jqXHR, textStatus, errorThrown){
-                console.log(textStatus,errorThrown);
-            },
-            complete: function(jqXHR, textStatus){
-                console.log("getData() Ajax Get Complete:", textStatus);
-            }
-        })
-    }
-    //
-    //function clearData(){
-    //    $data.empty();
-    //}
-
-    function processData(data){
-
-        for(var i = 0; i< data.length; i++){
-            var todo = data[i];
-            console.log(todo);
-
-            //var id = todo._id;
-            //var name = todo.name || '';
-            //var completed = todo.completed || false;
-            //var note = todo.note || '';
-            //
-            //var section = $('<section/>')
-            //    .attr('data-id', id);
-            //
-            //var ul = $('<ul/>')
-            //    .appendTo(section);
-            //
-            //var liName = $('<li/>')
-            //    .text('Name: ' + name)
-            //    .appendTo(ul);
-            //
-            //var liScore = $('<li/>')
-            //    .text('completed: ' + completed)
-            //    .appendTo(ul);
-            //
-            //var liDate = $('<li/>')
-            //    .text('Note: '+ note)
-            //    .appendTo(ul);
-            //
-            //$data.append(section);
-        }
-    }
-
-
-
-
-
-
-
-
 });
+
+//function postData(){
+//    $.ajax({
+//        url: '/people',
+//        method: 'post',
+//        data: {firstName: firstName, lastName: lastName, email: email},
+//        dataType: 'json',
+//        success: function(response, textStatus, jqXHR){
+//            //clearData();
+//            processData(response);
+//        },
+//        error: function(jqXHR, textStatus, errorThrown){
+//            console.log(textStatus,errorThrown);
+//        },
+//        complete: function(jqXHR, textStatus){
+//            console.log("postData() Ajax Post Complete:", textStatus);
+//        }
+//    })
+//}
+//
