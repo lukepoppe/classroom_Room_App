a2ef3927e53b8f92826da9264980790bb3149d99
lukepoppe <lukepoppe@hotmail.com>
2015-06-30 10:13:15 -0500

    instructorStatus

--- public/javascripts/helpModal.js
+++ public/javascripts/helpModal.js
@@ -6,48 +6,51 @@ function helpModal() {
 
     $('.yellow').click(function () {
         console.log("yellow Click");
-        $(".modalQuestionText").show();
-        $(this).css("background", "yellow");
-        $('.btn-primary').css("background", "yellow");
-        $('.btn-primary').css("color", "black");
-        $('.red').css("background", "white");
-        $('.green').css("background", "white");
-        $('.modal-header').css("background", "yellow");
-        $('.helpModalButton').css("background", "yellow");
-        $('#userImageDom').css("border-color", "yellow");
+            $(".modalQuestionText").show();
+            $(this).css("background", "yellow");
+            $('.btn-primary').css("background", "yellow");
+            $('.btn-primary').css("color", "black");
+            $('.red').css("background", "white");
+            $('.green').css("background", "white");
+            $('.modal-header').css("background", "yellow");
+            $('.helpModalButton').css("background", "yellow");
+            $('#userImageDom').css("border-color", "yellow");
+            $('.classRoomName').css("background", "yellow");
     });
 
     $('.red').click(function () {
         console.log("redClick");
-        $(".modalQuestionText").show();
-        $(this).css("background", "red");
-        $('.btn-primary').css("background", "red");
-        $('.btn-primary').css("color", "black");
-        $('.yellow').css("background", "white");
-        $('.green').css("background", "white");
-        $('.modal-header').css("background", "red");
-        $('.helpModalButton').css("background", "red");
-        $('.helpLevel').css("border-color", "red");
-        $('#userImageDom').css("border-color", "red");
+            $(".modalQuestionText").show();
+            $(this).css("background", "red");
+            $('.btn-primary').css("background", "red");
+            $('.btn-primary').css("color", "black");
+            $('.yellow').css("background", "white");
+            $('.green').css("background", "white");
+            $('.modal-header').css("background", "red");
+            $('.helpModalButton').css("background", "red");
+            $('.helpLevel').css("border-color", "red");
+            $('#userImageDom').css("border-color", "red");
+            $('.classRoomName').css("background", "red");
     });
 
     $('.green').click(function () {
         console.log("greenClick");
-        $(".modalQuestionText").hide();
-        $('.btn-primary').css("background", "green");
-        $('.btn-primary').css("color", "black");
-        $(this).css("background", "green");
-        $('.yellow').css("background", "white");
-        $('.red').css("background", "white");
-        $('.modal-header').css("background", "green");
-        $('.helpModalButton').css("background", "green");
-        $('.helpLevel').css("border-color", "green");
-        $('#userImageDom').css("border-color", "green");
+            $(".modalQuestionText").hide();
+            $('.btn-primary').css("background", "green");
+            $('.btn-primary').css("color", "black");
+            $(this).css("background", "green");
+            $('.yellow').css("background", "white");
+            $('.red').css("background", "white");
+            $('.modal-header').css("background", "green");
+            $('.helpModalButton').css("background", "green");
+            $('.helpLevel').css("border-color", "green");
+            $('#userImageDom').css("border-color", "green");
+            $('.classRoomName').css("background", "green");
     });
 
     $('.modal-footer').on('click', '.btn-primary', function () {
         console.log("submitButtonClick");
-        $('#statusModal').modal('hide');
+            $('#statusModal').modal('hide');
     });
 }
 
