1f72aee0977e3bb7deaf10b164ed1961aad33b71
mliq <liquori@gmail.com>
2015-06-30 09:21:54 -0500

    submit cohort and clearing fields

--- public/javascripts/cohorts.js
+++ public/javascripts/cohorts.js
@@ -101,6 +101,9 @@ function deleteCohortFromDB(id) {
 
 function submitPerson() {
     cohortsArray[cohortNumber].personArray.push(new Person($('#firstName').val(), $('#lastName').val(), $('#email').val(), "student"));
+    $('#firstName').empty();
+    $('#lastName').empty();
+    $('#email').empty();
     updateCohortInDB();
 }
 
@@ -200,8 +203,7 @@ function cohortPageInit() {
     $('.submitPerson').on("click", function () {
         console.log('submitPerson clicked');
         submitPerson();
-        $('.showList').show();
-        $('.showList').append("<li>" + cohortsArray[cohortNumber].personArray[click].firstName + " " + cohortsArray[cohortNumber].personArray[click].lastName + " | " + cohortsArray[cohortNumber].personArray[click].email + "   <button class='deletePerson'>Delete</button></li>");
-        click++;
+        //$('.showList').show();
+        //$('.showList').append("<li>" + cohortsArray[cohortNumber].personArray[click].firstName + " " + cohortsArray[cohortNumber].personArray[click].lastName + " | " + cohortsArray[cohortNumber].personArray[click].email + "   <button class='deletePerson'>Delete</button></li>");
     });
 }
