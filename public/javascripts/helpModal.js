8c0b4667f2b084df51fea0ea38bd8674525aac28
mliq <liquori@gmail.com>
2015-07-15 17:17:30 -0500

    Fixed all modals changing to status color

--- public/javascripts/helpModal.js
+++ public/javascripts/helpModal.js
@@ -11,7 +11,7 @@ function greenStatus() {
     $(this).css("background", "green");
     $('.yellow').css("background", "white");
     $('.red').css("background", "white");
-    $('.modal-header').css("background", "green");
+    $('.status-modal-header').css("background", "green");
     $('.helpModalButton').css("background", "green");
     $('.helpLevel').css("border-color", "green");
     $('#userImageDom').css("border-color", "green");
@@ -27,7 +27,7 @@ function yellowStatus() {
     //$('.btn-primary').css("color", "black");
     $('.red').css("background", "white");
     $('.green').css("background", "white");
-    $('.modal-header').css("background", "yellow");
+    $('.status-modal-header').css("background", "yellow");
     $('.helpModalButton').css("background", "yellow");
     $('#userImageDom').css("border-color", "yellow");
     help_status.flag = "yellow";
@@ -41,7 +41,7 @@ function redStatus() {
     //$('.btn-primary').css("color", "black");
     $('.yellow').css("background", "white");
     $('.green').css("background", "white");
-    $('.modal-header').css("background", "red");
+    $('.status-modal-header').css("background", "red");
     $('.helpModalButton').css("background", "red");
     $('.helpLevel').css("border-color", "red");
     $('#userImageDom').css("border-color", "red");
